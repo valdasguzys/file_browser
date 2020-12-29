f357f3b165111657263578fddbae517340245dbd
Valdas Gužys <valdas.guzys@gmail.com>
2020-12-29 21:23:42 +0200

    function to delete files and create folders fixed display of the current directory added 'back' button minor fixes

--- index.php
+++ index.php
@@ -7,41 +7,74 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<!-- TODO -->
-<!-- mygtukas katalogams trinti -->
-<!-- nuresetint paskutinio sukurto katalogo kintamuosius -->
-<!-- neteisingai rodo direktorija kurioje esame -->
-<!-- back mygtukas -->
-<!-- autentifikacija login passw -->
 
 
-<form action="" method="GET">
-<label for="new_folder">Create New Folder</label><br>
-<input type="text" id="mass" name="new_folder" value="New Folder"><br>
-<input type="submit" value="Submit" name="submit">
+<!-- back button php-->
+<?php 
+
+    // explode doesn't work properly on subfolder content. possibly missing "/" at the end off the path of the subfolder 
+
+    $current_dir_array = explode("/", $_SERVER['REQUEST_URI']);
+    $ending = array_splice($current_dir_array, count($current_dir_array) - 2);   
+    $url_back = implode("/", $current_dir_array);
+
+    print("<button><a href='$url_back'>BACK</a></button>");
+    
+    // //other way to get parrent folder. works ok
+
+    // $url_back2 = dirname($_SERVER['REQUEST_URI']);
+    // print("<button><a href='$url_back2'>BACK2</a></button>");
+?>
+
+<!-- new folder -->
+<form action="" method="POST">
+    <label for="new_folder">Create New Folder</label><br>
+    <input type="text" id="mass" name="new_folder" value="New Folder"><br>
+    <input type="submit" value="Submit" name="submit">
 </form>
 
-<?php 
-$new_folder = $_GET['new_folder'];
-if (is_dir($new_folder)) {
-        print('Folder ' . $new_folder . ' already exists!');
-    } else {
-        $create_folder = mkdir($new_folder);
-        print('Folder ' . $new_folder . ' was succesfuly created!');
+<!-- new folder php -->
+<?php
+    if (isset($_POST['new_folder'])) {
+        $new_folder = $_POST['new_folder'];
+            if (is_dir($new_folder)) {
+                print('Folder ' . $new_folder . ' already exists!');
+            } else if ($new_folder == "") {
+                print('Enter name for new folder.');
+            } else {    
+                $create_folder = mkdir($new_folder);
+                print('Folder ' . $new_folder . ' was succesfuly created!');
+        }
+    }
+    
+?>
+
+<!-- file deletion -->
+<?php
+    if (isset($_POST['delete'])) {
+        $delete_file = $_POST['delete'];
+            if (is_file($delete_file) == "") {
+                print('false');
+            } else {
+                unlink($delete_file);
+                print('File ' . $delete_file . ' succesfuly deleted!');
+            }
     }
 ?>
 
-
-<?php 
-    $path = './' . $_GET['path']; 
-    $scan = scandir($path);                      
-?>
-
+<!-- current folder -->
 <h3>
     <?php 
-    print($_SERVER['REQUEST_URI'] . " katalogo turinys");
+    $current_folder = str_replace('?path=', '' ,$_SERVER['REQUEST_URI']); 
+    print($current_folder . ' folder contents');
     ?>
 </h3>
+
+<!-- display folder contents -->
+<?php
+    $path = './' . $_GET['path']; 
+    $scan = scandir($path);                     
+?>
 
 <table>
     <tr>
@@ -50,18 +83,32 @@ if (is_dir($new_folder)) {
     <th>Action</th>
 
         <?php 
-            for ($i = 0; $i <= count($scan) - 1; $i++ ) {
+            for ($i = 2; $i <= count($scan) - 1; $i++ ) {
                 if (is_dir($scan[$i])) {
-                    print('<tr><td><a href="' . $_SERVER['REQUEST_URI'] . '?path=' . $scan[$i] . '/' . '">' . $scan[$i] . '</a></td><td>Folder</td><td></td></tr>');
+                    print('<tr><td>
+                    <a href="' . $_SERVER['REQUEST_URI'] . '?path=' . $scan[$i] . '/' . '">' . $scan[$i] . '</a></td>
+                    <td>Folder</td>
+                    <td></td>
+                    </tr>');
                 } else {
                     print('<tr><td>
-                    <a href="' . $_SERVER['REQUEST_URI'] . $scan[$i] . '">' . $scan[$i] . '</a></td><td>File</td><td><button>delete</button></td></tr>');
-                    }
+                    <a href="' . $_SERVER['REQUEST_URI'] . $scan[$i] . '">' . $scan[$i] . '</a></td>
+                    <td>File</td>
+                    <td>
+                    <form action="index.php" method="POST">
+                    <input type="hidden" name="delete" value="' . $scan[$i] . '">
+                    <button type="submit">Delete</button>
+                    </form>
+                    </td>
+                    </tr>');
+                }
             }                 
-        ?>
-        
+        ?>        
     </tr>
 </table>
+
+
+
 
 </body>
 </html>
